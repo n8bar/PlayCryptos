4b6c57fde821dc1ea83e7df7fa828ac1d3aec82f
Nate Barlow <n8barlow@gmail.com>
2024-01-26 20:24:29 -0700

    views

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
+
+app.set('view engine', 'ejs');
+
 const PORT = process.env.PORT || 80
 
-app.get('/', (req, res) => {
+app.get('/helloX', (req, res) => {
     res.send('Hello Express.js!');
+});
+
+app.get('/', (req, res) => {
+    res.render('index', { message: 'Hello Express.js!!' });
+});
+
+app.use((req, res, next) => {
+    res.status(404).render('404', { url: req.originalUrl });
 });
 
 app.listen(PORT, () => {
     console.log(`running on port ${PORT}`);
 });
+
